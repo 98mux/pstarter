adc4e3c8c46bc382fc1ec50a72e9ae391c3942cc
filipot <filipot@stud.ntnu.no>
2020-04-07 01:15:50 +0200

    inital

---
+++ src/routes/home/index.js
@@ -0,0 +1,12 @@
+import { h } from "preact";
+import style from "./style";
+
+const Home = () => (
+    <div class={style.home}>
+        <h1>Home</h1>
+        <p>This is one of the Home component.</p>
+        <p> Testing</p>
+    </div>
+);
+
+export default Home;
